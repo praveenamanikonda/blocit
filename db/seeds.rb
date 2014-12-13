9b0b663dd230573bab28a98b6ee30cd6ee1f9c30
praveenamanikonda <praveena.manikonda@gmail.com>
2014-12-13 07:01:44 +0000

    Associating users with posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,29 @@
 require 'faker'
 
+# Create Users
+ 5.times do
+   user = User.new(
+     name:     Faker::Name.name,
+     email:    Faker::Internet.email,
+     password: Faker::Lorem.characters(10)
+   )
+   user.skip_confirmation!
+   user.save!
+ end
+ users = User.all
+
+# Note: By calling 'User.new' instead of 'create',
+# We create an instance of user which isn't immediately saved to the database.
+
+# The `skip_confirmation!` method sets the `confirmed_at` attribute
+# to avoid triggering an confirmation email when the User is saved.
+
+# The `save` method then saves this User to the database.
+
 # Create Posts 
 50.times do 
   Post.create!(
+    user: users.sample,
     title: Faker::Lorem.sentence,
     body: Faker::Lorem.paragraph
     )
@@ -12,12 +33,21 @@ posts = Post.all
 #Create Comments 
 100.times do 
   Comment.create!(
+    #    user: users.sample,   # we have not yet associated Users with Comments 
     post: posts.sample,
     body: Faker::Lorem.paragraph
     )
 end   
 
+user = User.first
+ user.skip_reconfirmation!
+ user.update_attributes!(
+   email: 'veena.chaitu2007@gmail.com',
+   password: 'helloworld'
+ )
+
 puts "Seeds finished"
+puts "#{User.count} users created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} posts created"
  
