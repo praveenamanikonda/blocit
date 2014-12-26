cde36bdf740385a960517050505f976e6db0a288
praveenamanikonda <praveena.manikonda@gmail.com>
2014-12-26 07:59:21 +0000

    Deleting topics , posts and comments

--- app/policies/comment_policy.rb
+++ app/policies/comment_policy.rb
@@ -5,5 +5,9 @@ class CommentPolicy < ApplicationPolicy
   
   def create?
     user.present? || user.admin?
+  end  
+  
+  def destroy?
+    user.present? && (record.user == user || user.admin? || user.moderator?)
   end   
 end   
--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -1,5 +1,9 @@
 class PostPolicy < ApplicationPolicy
   def index?
     true 
+  end  
+  
+  def destroy 
+    user.present? && (record.user == user || user.admin? || user.moderator?)
   end   
 end   
