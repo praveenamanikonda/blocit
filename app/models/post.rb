5168cdffe6e48030e9dfae8807f64f43cc40dc06
praveenamanikonda <praveena.manikonda@gmail.com>
2015-01-05 19:54:22 +0000

    mocking with rspec

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,12 +6,11 @@ class Post < ActiveRecord::Base
   
   mount_uploader :image, ImageUploader
   default_scope { order('rank DESC') }
-  after_create :create_vote 
   
   #validates :title, length: {minimum: 5}, presence: true 
   #validates :body, length: {minimum: 20}, presence: true 
-  #validates :topic, presence: true 
-  #validates :user, presence: true 
+  validates :topic, presence: true 
+  validates :user, presence: true 
   
   def up_votes
     votes.where(value: 1).count
@@ -32,9 +31,6 @@ class Post < ActiveRecord::Base
     
     update_attribute(:rank, new_rank)
   end   
-  
- 
-  private 
   
   def create_vote
     user.votes.create(value: 1, post: self)
--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -1,6 +1,9 @@
 require 'rails_helper'
 
-describe Vote do 
+describe Vote do
+  
+  include TestFactories
+  
   describe "validations" do 
     before do 
       @v = Vote.new(value: 1)
@@ -15,5 +18,14 @@ describe Vote do
         expect( @v2.valid?).to eq(false)
       end   
     end    
+  end 
+  
+  describe "after_save" do
+    it "calls 'Post#update_rank' after save" do 
+      post = associated_post
+      vote = Vote.new(value: 1, post: post)
+      expect(post).to receive(:update_rank)
+      vote.save
+    end   
   end   
-end   
+end
