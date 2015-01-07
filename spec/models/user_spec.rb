e43d1f6114f6b4b4f2aec223385dff41ab1d1e35
praveenamanikonda <praveena.manikonda@gmail.com>
2015-01-07 20:36:56 +0000

    Added user spec to favoriting feature

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,10 +5,20 @@ describe User do
   include TestFactories 
   
   describe "#favorited(post)" do 
-    xit "returns `nil` if the user has not favorited the post" do 
+    
+    before do 
+      @post = associated_post 
+      @user = authenticated_user 
+      #@favorite = Favorite.new(post: @post, user: @user )
     end 
     
-    xit "returns appropriate favorite if it exists" do 
+    it "returns `nil` if the user has not favorited the post" do
+      expect(@user.favorited(@post)).to eq(nil)
+    end 
+    
+    it "returns appropriate favorite if it exists" do
+      @user.favorites.where(post: @post).create
+      expect(@user.favorited(@post).post).to eq(@post)  
     end   
   end  
 end 
