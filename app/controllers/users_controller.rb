b0f1f71950727520d7c11d81a048f940dfa84b5f
praveenamanikonda <praveena.manikonda@gmail.com>
2015-01-12 20:43:58 +0000

    Popular posts and performance improvements

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,10 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @post = @user.posts.visible_to(current_user)
     @comment = @user.comments 
+  end
+  
+  def index
+    @users = User.top_rated.paginate(page: params[:page], per_page: 10)
   end   
  
    private
