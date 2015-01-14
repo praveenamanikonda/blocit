86ca9c4042d1e3a9edb1e585a112d3eb2a3e3df4
praveenamanikonda <praveena.manikonda@gmail.com>
2015-01-14 22:31:50 +0000

    Deleting comments with ajax

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,5 @@
 class CommentsController < ApplicationController
+  respond_to :html, :js
   def create 
     @post = Post.find(params[:post_id])
     @topic = @post.topic
@@ -24,10 +25,12 @@ class CommentsController < ApplicationController
     
     if @comment.destroy 
       flash[:notice] = "Comment was removed"
-      redirect_to [@topic, @post]
     else 
       flash[:error] = "Error deleting the comment"
-      redirect_to [@topic, @post]
+    end  
+    
+    respond_with(@comment) do |format|
+      format.html { redirect_to [@post.topic, @post] }
     end   
   end 
   
